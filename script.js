c0a7af671ad259306eeaa7a20330170bf49e2db5
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-09-07 21:43:09 +0300

    finding the max number

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function findMax(...num) { // to pass any number of numbers to the function
 		}
 	}
 
-	document.write(`Найбільше число: ${max}`); // outputing the result on the page  containing max number
+	document.write(`The largest number: ${max}`); // result is displayed on the page
 }
 
 findMax(2, 16, 15, 8, 25); // calling findMax function
