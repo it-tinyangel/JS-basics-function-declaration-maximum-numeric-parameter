96dbdc1e31d0e2135e810b0497a21a3c75a90c0d
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-09-07 21:37:56 +0300

    explicitly passing arguments to a function
    here the function accepts any number of arguments (specified by the unpacking operator ...num) and finds the largest number of them

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function findMax() { // finding the maximum number
+function findMax(...num) { // to pass any number of numbers to the function
 
-	let max = Number.NEGATIVE_INFINITY; // checking to define a minimum value
 
-	for (let i = 0; i < arguments.length; i++) { // iterating through all the arguments passed to the function through arguments
+	let max = num[0]; // an initial value of max to compare
 
-		if (arguments[i] > max) { // if the (arguments[i] is greater than max, then max is updated with the value of the argument
-			max = arguments[i];
+	for (let i = 0; i < num.length; i++) { // iterating through all the passed arguments in the num array
+
+		if (num[i] > max) { // if num[i] is greater than max, then max is updated with the value of num[i]
+			max = num[i];
 		}
 	}
 
-	return max; // the function returns the maximum value max found
+	document.write(`Найбільше число: ${max}`); // outputing the result on the page  containing max number
 }
 
-document.write(`Найбільше число: ${findMax(2, 18, -2, 0, 21, 3)}`); // result is displayed on the page
+findMax(2, 16, 15, 8, 25); // calling findMax function
