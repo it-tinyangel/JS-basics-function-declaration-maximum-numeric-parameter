e1f20bd7ea21dee482c6917f34e48c97602d9c03
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-09-07 21:52:08 +0300

    finding the max number using arguments

---
+++ script (with arguments).js
@@ -0,0 +1,15 @@
+function findMax() { // the function finding the maximum number
+
+	let max = Number.NEGATIVE_INFINITY; // checking to define a minimum value
+
+	for (let i = 0; i < arguments.length; i++) { // iterating through all the arguments passed to the function through arguments
+
+		if (arguments[i] > max) { // if the (arguments[i] is greater than max, then max is updated with the value of the argument
+			max = arguments[i];
+		}
+	}
+
+	return max; // the function returns the maximum value max found
+}
+
+document.write(`Найбільше число: ${findMax(2, 18, -2, 0, 21, 3)}`); // result is displayed on the page
